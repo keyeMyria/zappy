cccd30f20186f68665c6f7611d6f9ebca8bf8287
Ahmed Maher <a.maher@cubeegypt.com>
2018-04-08 21:37:18 +0200

    Service:   - minor config to enable Cross Origin requests     for the angular app to be able to call the api
    modified:   service/conf/settings/production.py
    modified:   service/env
    modified:   service/requirements/production.txt

--- service/conf/settings/production.py
+++ service/conf/settings/production.py
@@ -3,4 +3,11 @@ from conf.settings.common import *
 
 INSTALLED_APPS += [
     'gunicorn',
+    'corsheaders',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = env.bool('DJANGO_CORS_ORIGIN_ALLOW_ALL', default=False)
+CORS_ALLOW_CREDENTIALS = env.bool('DJANGO_CORS_ALLOW_CREDENTIALS', default=False)
+CORS_ORIGIN_WHITELIST = env.list('DJANGO_CORS_ORIGIN_WHITELIST', default=[])
+
+MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
